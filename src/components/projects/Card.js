ef3abc78c0329362b94ab77520d004a95acfe8c4
KDSGYT <kdsgyt@gmail.com>
2020-10-29 20:12:59 -0400

    Added Features: new Project Show System with advance scalability Removed features: Old Project Show System

---
+++ src/components/projects/Card.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Card.scss'
+function Card({ projectName, projectImage, technologiesUsed, links }) {
+
+    const technologiesUsedList = technologiesUsed.map((tech) => {
+        return (<li>{tech}</li>);
+    })
+
+    const { github, website } = links;
+
+    return (
+        <div className="card">
+            <img className="project-image" src={projectImage} alt="projectImage" />
+            <h1>{projectName}</h1>
+            {/* <h3>Tech used</h3> */}
+            <ul class="technologies-list">
+                {technologiesUsedList}
+            </ul>
+            <ul className="usefulLinks">
+                <li><a href={github} target="_blank">Github</a> </li>
+                <li><a href={website} target="_blank">Website</a> </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Card;
