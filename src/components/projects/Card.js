3601b24b7d695a963779f9988328127c6e836c25
KDSGYT <kdsgyt@gmail.com>
2020-12-07 23:09:35 -0500

    New Card updated

--- src/components/projects/Card.js
+++ src/components/projects/Card.js
@@ -1,26 +1,41 @@
 import React from 'react'
+// import { SvgIcon } from '@material-ui/core/SvgIcon';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import VisibilityRoundedIcon from '@material-ui/icons/VisibilityRounded';
 import './Card.scss'
 
 function Card({ projectName, projectImage, technologiesUsed, links }) {
-
-    const technologiesUsedList = technologiesUsed.map((tech, index) => {
-        return (<li key={index}>{tech}</li>);
-    })
 
     const { github, website } = links;
 
     return (
         <div className="card">
-            <img className="project-image" src={projectImage} alt="projectImage" />
+            <div className="upper-part">
+                <img className="project-image" src={projectImage} alt="projectImage" />
+
+            </div>
             <h1>{projectName}</h1>
-            {/* <h3>Tech used</h3> */}
-            <ul className="technologies-list">
-                {technologiesUsedList}
-            </ul>
-            <ul className="usefulLinks">
-                <li key="github"><a href={github} rel="noopener noreferrer" target="_blank">Github</a> </li>
-                <li key="website"><a href={website} rel="noopener noreferrer" target="_blank">Website</a> </li>
-            </ul>
+            <div className="usefulLinks lower-part">
+
+                <button className="github" onClick={() => window.open(github)}>
+                    <GitHubIcon
+                        className="icon"
+                        key="github"
+                        style={{ fontSize: "3em", }}
+
+                    />
+                </button>
+                <button className="website" onClick={() => window.open(website)}>
+                    <VisibilityRoundedIcon
+                        className="icon"
+
+                        key="website"
+                        style={{ fontSize: "3.7em" }}
+                    />
+                </button>
+                {/* <li key="github"><a href={github} rel="noopener noreferrer" target="_blank">Github</a> </li> */}
+                {/* <li key="website"><a href={website} rel="noopener noreferrer" target="_blank">Website</a> </li> */}
+            </div>
         </div>
     )
 }
