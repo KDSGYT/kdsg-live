b500571c0e319320179ed46b214a7e752cb1eed4
KDSGYT <kdsgyt@gmail.com>
2020-12-13 15:44:41 -0500

    new Skills section added

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import Card from '../card/Card';
 import './About.scss';
 import '../Transition.scss';
-
+import frontEnd from '../../img/SVG/web-design.svg';
+import backEnd from '../../img/SVG/backend.svg';
+import software from '../../img/SVG/computer.svg';
 const About = () => {
 
     const [render, setrender] = useState(false)
@@ -19,18 +21,58 @@ const About = () => {
         }
     }, [])
 
-    
 
 
 
-    const content = render ? (<Card render={render}/>) : <div></div>;
+
+    // const content = render ? (<Card render={render} />) : <div></div>;
 
     return (
-        <section className="about">
+        <section id="about">
             <div className="content">
                 <h1>ABOUT</h1>
                 <p id="me">I am self-taught web developer located in Brampton, ON. I love problem solving and making new stuff. I like to push myself to think out of the box.</p>
-                    {content}
+            </div>
+            <h2>Skills</h2>
+            <div id="skills-section">
+                <Card
+                    id="front-end"
+                    sectionName="Front-End"
+                    content={[
+                        "HTML",
+                        "CSS",
+                        "JavaScript",
+                        "ReactJS"
+                    ]}
+                    render={render}
+                    SVG={frontEnd}
+                />
+                <Card
+                    id="backend"
+                    sectionName="Back-End"
+                    content={[
+                        "Firebase",
+                        "NodeJS",
+                        "MySQL",
+                        "ExpressJS"
+                    ]}
+                    render={render}
+                    SVG={backEnd}
+
+                /><Card
+                    id="software"
+                    sectionName="Software"
+                    content={[
+                        "VSCode",
+                        "Figma",
+                        "Adobe Photoshop",
+
+                    ]}
+                    render={render}
+                    SVG={software}
+
+                />
+                {/* {content}/ */}
             </div>
 
         </section>
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,46 +1,38 @@
 import React from 'react';
-import HTML5 from '../../img/SVG/html-5.svg';
-import CSS3 from '../../img/SVG/css-3.svg';
-import ReactJS from '../../img/SVG/reactjs.svg';
-import JavaScript from '../../img/SVG/javascript.svg';
-import NodeJs from '../../img/SVG/nodejs.svg';
-// import from '../../';
 import './Card.scss';
 import CSSTransitionGroup from 'react-transition-group/CSSTransition';
 
 
-function Card({ render }) {
+function Card({ render, id, sectionName, SVG, content }) {
 
 
     React.useEffect(() => {
         setAnimate(true)
-        return () => {
-            //cleanup
-        }
     }, [render])
 
 
     const [animate, setAnimate] = React.useState(false)
-
+    const skills = content.map(skill => <li>{skill}</li>)
     return (
 
         <CSSTransitionGroup
+            id={id}
             classNames="fade"
             timeout={2000}
             in={animate}
         >
 
-            <div className="my-skillset">
-                <div id="skills">
-                    <img src={HTML5} alt="" />
-                    <img src={CSS3} alt="" />
-                    <img src={ReactJS} alt="" />
-                    <img src={JavaScript} alt="" />
-                    <img src={NodeJs} alt="" />
+            <div className="skill-card">
+                <div className="overlay-flap">
+                    <img className="inner-container" src={SVG} alt={sectionName} />
                 </div>
-
-
+                <h1 className="section-name">{sectionName}</h1>
+                <ul className="skills">
+                    {skills}
+                </ul>
             </div>
+
+
 
         </CSSTransitionGroup>
     )
