abfbab99f7562aadefda90c9e31fc0ae6705bd71
Karan Pal Singh <kdsgyt@gmail.com>
2021-01-02 00:21:57 -0500

    Added firebase configuration

--- src/Util/firebase.ts
+++ src/Util/firebase.ts
@@ -4,12 +4,12 @@ import 'firebase/storage'
 
 const config = {
     apiKey: "AIzaSyBJ8yfKyUzRCYj_1BLq2cw1R0C3JR1ZXTM",
-    authDomain: "kdsg-live.firebaseapp.com",
-    projectId: "kdsg-live",
-    storageBucket: "kdsg-live.appspot.com",
-    messagingSenderId: "652675755195",
-    appId: "1:652675755195:web:cb373dd5c68dde2bb28d9e",
-    measurementId: "G-5RCMYQ9JXR"
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGIN_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_Measurement_ID
   };
 
 firebase.initializeApp(config);
