be5cd1b887af050ea7c550f6b78b27d4e000e784
Karan Pal Singh <kdsgyt@gmail.com>
2021-01-03 19:06:38 -0500

    Removed static files

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import "./Projects.scss";
 import Card from './Card';
-import SubsCount from '../../img/subCount.png'
-import Way2Way from '../../img/way2way.png'
-import firebaselogin from '../../img/firebase-login.png';
-import { getProjectImage, getProjects } from '../../assets/Functions';
+import { getProjects } from '../../assets/Functions';
 
 function Projects() {
 
@@ -12,37 +9,9 @@ function Projects() {
     useEffect(() => {
         console.log('runned')
         getProjects(setProjectsData)
-    },[]);
-    // const cards = [
-    //     {
-    //         name: "SubsCount",
-    //         projectImage: SubsCount,
-    //         links: {
-    //             github: "https://github.com/KDSGYT/subsCount-react",
-    //             website: "https://kdsgyt.github.io/subsCount-react/"
-    //         }
-    //     },
-    //     {
-    //         name:"Way2Way",
-    //         projectImage: Way2Way,
-    //         links:{
-    //             github: "https://github.com/KDSGYT/way2way-react",
-    //             website: "http://way2way.in"
-    //         }
-
-    //     },
-    //     {
-    //         name:"Firebase Authentication",
-    //         projectImage: firebaselogin,
-    //         links:{
-    //             github: "https://github.com/KDSGYT/firebase-login",
-    //             website: "http://firebase-login.kdsg.live"
-    //         }
-    //     }
-    // ]
+    }, []);
 
     const cardData = projectsData.map(({ name, projectImage, links }, index) => {
-        // const image = getProjectImage(projectImage);
 
         return <Card
             projectName={name}
