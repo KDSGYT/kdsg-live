0332d7e27d1aa7b7cdd27a370cc7c9a269c23e48
Karan Pal Singh <kdsgyt@gmail.com>
2021-02-05 13:58:49 -0500

    sdf

--- src/components/ToggleButton/ToggleButton.tsx
+++ src/components/ToggleButton/ToggleButton.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { FC, useEffect } from 'react';
+import './ToggleButton.scss';
 
-interface props{
-    toggle:any,
-    setToggle:any
+interface props {
+    toggle: string
+    setToggle: any
 }
+const ToggleButton: FC<props> = ({ toggle, setToggle }) => {
 
-const ToggleButton:React.FC<props> = ({toggle, setToggle}) =>{
+
+    // Toggle the button
+    function handleToggle() {
+        if (toggle === "") setToggle("toggle")
+        else setToggle("")
+    }
+
     return (
-        <button id="toggle-button" onClick={() => toggle ? setToggle(false) : setToggle(true)}>
-            <span className="button-line" id="line-1"><hr /></span>
-            <span className="button-line" id="line-2"><hr /></span>
-            <span className="button-line" id="line-3"><hr /></span>
-        </button>
+        <div id="button-container">
+            <button id="toggle-button" className={toggle} onClick={handleToggle}>
+                <hr className={"line line1"}></hr>
+                <hr className={"line line2 "}></hr>
+                <hr className={"line line3"}></hr>
+            </button>
+        </div>
     )
-
 }
-
 export default ToggleButton;
