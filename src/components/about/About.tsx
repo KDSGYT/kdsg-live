8c5ec3c2dd9d6e3a1b82abafcac83231f5462c21
Karan Pal Singh <kdsgyt@gmail.com>
2021-02-19 18:53:55 -0500

    New About section completed

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,15 +1,28 @@
 import React, { useState, useRef } from 'react';
 import './About.scss';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-
+import DP from '../../img/dp.PNG';
+import { Button } from '@material-ui/core'
 const Contact = () => {
     return (
         <section id="contact">
 
-            <div id="about-card">
-                <img src=""/>
+            {/* Display flex is the global styling class */}
+            <div id="about-card" className="flex-display">
+                <div id="main-section" className="flex-display">
+                    <img src={DP} />
+                    <h1>Karan Pal Singh</h1>
+                </div>
+
+                <p>Hi there 👋, <br />I am a self-taught web developer.</p>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => window.location.href= "mailto:kdsgyt@gmail.com"}
+                >
+                    Hire Me!
+                </Button>
             </div>
+
         </section>
     )
 }
