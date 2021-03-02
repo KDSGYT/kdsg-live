66fba1243147e02cdafa18921412725f554973ff
Karan Pal Singh <kdsgyt@gmail.com>
2021-03-02 11:08:00 -0500

    Transformed to gatsby

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/home/celvin/Desktop/new folder/kdsg-live/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/home/celvin/Desktop/new folder/kdsg-live/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/home/celvin/Desktop/new folder/kdsg-live/src/pages/index.js")),
   "component---src-pages-page-2-js": preferDefault(require("/home/celvin/Desktop/new folder/kdsg-live/src/pages/page-2.js")),
