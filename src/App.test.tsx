05e523a7e83bd072bb3b695dc3a5a2816bb711df
Karan Pal Singh <kdsgyt@gmail.com>
2021-03-02 11:03:46 -0500

    removed everything and installed gatsby project

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('app title should be "Portfolio | Home" ', async () => {
-    await render(<App />)
-    expect(document.title).toEqual("Portfolio | Home")
-});
--- src/components/photoGrid/PhotoGrid.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function PhotoGrid() {
-    return(
-        <section id="photo-grid">
-            
-        </section>
-    )
-}
