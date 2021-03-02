2d37cec5d12c5e37df79a33e14f266d0e6eb411b
Karan Pal Singh <kdsgyt@gmail.com>
2021-03-02 11:24:43 -0500

    learning gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-typescript`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
