3c5286b056f01478e6702f0e224ceeba11bf5ad1
Karan Pal Singh <kdsgyt@gmail.com>
2021-03-05 11:57:22 -0500

    Changed the email until further notice

--- src/components/ContactUS/ContactUs.tsx
+++ src/components/ContactUS/ContactUs.tsx
@@ -7,7 +7,7 @@ export default function ContactUs() {
         <section id="contact-us" className="display-flex">
             <h1>Contact Me</h1>
             <p>If you’re hiring for a Frontend role, or have any questions,  please don’t hesitate to reach out!</p>
-            <h2>karan@KDSG.LIVE</h2>
+            <h2>kdsgyt@gmail.com</h2>
             <SocialMediaLinks />
         </section>
     )
