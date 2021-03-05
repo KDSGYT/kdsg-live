d824293eb13064ef8cf4728083c014ec27b38a83
Karan Pal Singh <kdsgyt@gmail.com>
2021-03-05 11:14:48 -0500

    Fixed the linkes not showing in the production version of the website

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@
 require("dotenv").config()
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `KDSG | Front End Developer`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@KDSGYT`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/navbar/Links/Links.tsx
+++ src/components/navbar/Links/Links.tsx
@@ -2,16 +2,16 @@ import * as React from 'react';
 import './Links.scss';
 import scrollTo from 'gatsby-plugin-smoothscroll';
 import resume from '../../assets/Resume.pdf';
+import { CSSTransition } from 'react-transition-group';
+
 
 export default function Links({toggle, setToggle}) {
     
-    const [appear, setAppear] = React.useState(toggle)
+    const [appear, setAppear] = React.useState(toggle);
+
     React.useEffect(() =>{
         setAppear(toggle === "toggle" ? "appear": "")
     },[toggle])
-    React.useEffect(() => {
-        console.log(appear)
-    },[appear])
 
     function handleClick(where) {
         scrollTo('#'+where)
@@ -21,7 +21,9 @@ export default function Links({toggle, setToggle}) {
 
     return (
         <ul id={appear} className={`links`}>
-            <li onClick={() => handleClick('home')}id="link-1">Home</li>
+            <li onClick={() => handleClick('home')}id="link-1">
+                    Home
+            </li>
             <li onClick={() => handleClick('work')}id="link-2">Work</li>
             <li onClick={() => handleClick('contact-us')}id="link-3">Contact Me</li>
             <li id="link-4"><a href={resume} target="_blank">Resume</a></li>
