8cfbf7154c8c98332668c1d719403b6637954787
Karan Pal Singh <kdsgyt@gmail.com>
2021-03-05 11:17:36 -0500

    Added google analytics to the app

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,3 +7,4 @@
 // You can delete this file if you're not using it
 import "firebase/app"
 import "firebase/firestore";
+import "firebase/analytics"
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,5 +6,6 @@
 
 // You can delete this file if you're not using it
 
-import 'firebase/app'
+import 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/analytics';
