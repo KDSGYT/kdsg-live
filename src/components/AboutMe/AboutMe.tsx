54e20e53628af10aa5e5fd82858e6cfbea066702
Karan Pal Singh <kdsgyt@gmail.com>
2021-03-31 10:08:56 -0400

    Changed styles: 1. Removed Excess Lines from about me description. 2. Added consistent fonts faces to headings 3. Added max-width for for the body so that it won't bleed when the screen size bigger than normal

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -6,12 +6,9 @@ export default function AboutMe() {
     return (
         <section id="about-me" className="display-flex">
             <div id="about-section" >
-                <h2>About Me</h2>
+                <h2>ABOUT ME</h2>
                 <p>
                     My name is Karan. I'm a passionate Frontend Web developer using web technologies to build amazing products and focusing on solving problems for different niches and different industries using the power of technology.
-                <br />
-                    <br />
-                I will love to hear from you. Whether it's a project, job opportunity, or just a chat. Feel free to contact me.
                 </p>
             </div>
             <div id="skills-section" >
