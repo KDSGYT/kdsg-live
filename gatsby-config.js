e16dc00baceb85a1c74bd692273ec844461369b6
Karan Pal Singh <kdsgyt@gmail.com>
2022-04-09 13:43:25 -0400

    1. Updated README 2. Removed the database url from gatsby configuration that is not required to run the app and was creating difficulty starting the app

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,6 @@ module.exports = {
         credentials: {
           apiKey: process.env.GATSBY_FIREBASE_API_KEY,
           authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
-          databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
           projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
           storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
           messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
