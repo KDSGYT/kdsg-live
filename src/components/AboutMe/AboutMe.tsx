4ed398aa00377074b0f8801ce076a50d185f94a5
Karan Pal Singh <kdsgyt@gmail.com>
2022-04-16 16:26:37 -0400

    Following data is being dynamically retrieved from firebase DB. 1. About Us data 2. Skills data and is sorted using the bubble sort algorithm

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -2,21 +2,25 @@ import * as React from 'react';
 import Parallax from '../Animate/Parallax';
 import './AboutMe.scss';
 import firebase from 'gatsby-plugin-firebase'
+import { bubbleSort } from '../assets/functions';
 
+interface SkillsData {
+}
 
 export default function AboutMe() {
 
     const aboutSection = React.useRef<any>()
     const skills = React.useRef<any>()
     const [aboutText, setAboutText] = React.useState<string>("")
-    
+    const [skillsData, setSkillsData] = React.useState<Array<Object>>([{}]);
+
+    // Get about me from DB
     React.useEffect(() => {
         firebase
         .firestore()
         .collection('/about-text')
         .get()
         .then(res => {
-            console.log(res)
             setAboutText(() => {
                 let newState;
                 res.forEach(item => newState = item.data().data)
@@ -24,7 +28,24 @@ export default function AboutMe() {
             })
         })
         .catch((e) => console.error(e.message) )
-    })
+        return (() => setAboutText(""))
+    },[]);
+
+    // Get skills data from DB
+    React.useEffect(() => {
+        firebase.firestore()
+        .collection('/skillset')
+        .get()
+        .then(res => {
+            setSkillsData(() => {
+                const data = [];
+                res.forEach(item => data.push(item.data()))
+                // sort the data using bubble sort algorithm
+                return bubbleSort(data)
+            })
+        })
+        return(() => setSkillsData([]))
+    },[]);
 
     return (
         <section ref={aboutSection} id="about-me" className="display-flex">
@@ -49,18 +70,15 @@ export default function AboutMe() {
                 animationDelay={1}
                 className='animation'
             >
+                {/* create a varible that will represent the serial number of the skills in which they willbe displayed in the skills section of the website */}
                 <div ref={skills} id="skills-section" >
                     <h2>SKILLS</h2>
                     <ol className="display-flex">
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>React</li>
-                        <li>Angular</li>
-                        <li>GraphQL</li>
-                        <li>Gatsby</li>
-                        <li>Firebase</li>
-                        <li>Node.js</li>
+                        {skillsData.map((item:any) => {
+                            return (
+                                <li>{item.value}</li>
+                            )
+                        })}
                     </ol>
                 </div>
             </Parallax>
