a7d77a01d6f49acb7c61af143ba22ef2794626dc
Karan Pal Singh <kdsgyt@gmail.com>
2022-08-07 01:54:44 -0400

    Searchbar is done. Need to add functionality to the bar

--- src/components/Work/searchbar/SearchBar.tsx
+++ src/components/Work/searchbar/SearchBar.tsx
@@ -18,15 +18,34 @@ import './SearchBar.scss';
  */
 export const Searchbar: React.FC<{}> = () => {
 
+    const [moveLines, setMoveLines] = React.useState<Boolean>(false)
+    const leftLine:any = React.useRef(null)
+    const rightLine:any = React.useRef(null)
+    const inputField = React.useRef(null)
+    // Funciton to change the state focus on input changes
+    function changeState():void { setMoveLines((prevState) => prevState ? false : true) }
+    React.useEffect(() => {
+    
+        if(moveLines){
+            rightLine.current.style.width= "0";
+            leftLine.current.style.width= "0";
+        } else if(moveLines===false) {
+            rightLine.current.style.width= "50%";
+            leftLine.current.style.width= "50%";
+
+        }
+    }, [moveLines])
+    
     return (
         <div id="search-bar">
             <label for="search-input">
-                {/* <img src='./search.svg' /> */}
                 <div id="image-container">
                     <img src="https://img.icons8.com/ios-filled/50/000000/search--v1.png" />
 
                 </div>
-                <input type="text" id="search-input" placeholder='Skill, Technology, Project...' autoComplete='off' />
+                <div ref={leftLine} id="left-line"></div>
+                <input ref={inputField} onFocus={changeState} onBlur={changeState} type="text" id="search-input" placeholder='Skill, Technology, Project...' autoComplete='off' />
+                <div ref={rightLine} id="right-line"></div>
             </label>
         </div>)
 }
