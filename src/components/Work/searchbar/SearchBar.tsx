3e589a6bf7dc568cce2194b4d6ef712c89dea1bd
Karan Pal Singh <kdsgyt@gmail.com>
2022-08-08 23:18:08 -0400

    Updated the searchbar component

--- src/components/Work/searchbar/SearchBar.tsx
+++ src/components/Work/searchbar/SearchBar.tsx
@@ -4,7 +4,7 @@ import './SearchBar.scss';
  * Search bar to search through all the projects.
  * Approach:
  * Tags: 
- * Serial Number
+ * Serial Number: It will be using bubblesort to sort all the projects.
  * 
  *  
  */
@@ -19,32 +19,48 @@ import './SearchBar.scss';
 export const Searchbar: React.FC<{}> = () => {
 
     const [moveLines, setMoveLines] = React.useState<Boolean>(false)
-    const leftLine:any = React.useRef(null)
-    const rightLine:any = React.useRef(null)
-    const inputField = React.useRef(null)
+    const leftLine: any = React.useRef(null)
+    const rightLine: any = React.useRef(null)
+    const inputField: any = React.useRef(null)
+
+    const [searchTerm, setSearchTerm] = React.useState<String>('')
     // Funciton to change the state focus on input changes
-    function changeState():void { setMoveLines((prevState) => prevState ? false : true) }
+    function changeState(): void { setMoveLines((prevState) => prevState ? false : true) }
     React.useEffect(() => {
-    
-        if(moveLines){
-            rightLine.current.style.width= "0";
-            leftLine.current.style.width= "0";
-        } else if(moveLines===false) {
-            rightLine.current.style.width= "50%";
-            leftLine.current.style.width= "50%";
+
+        if (moveLines) {
+            rightLine.current.style.width = "0";
+            leftLine.current.style.width = "0";
+        } else if (moveLines === false) {
+            rightLine.current.style.width = "50%";
+            leftLine.current.style.width = "50%";
 
         }
     }, [moveLines])
-    
+
+
+    React.useEffect(() => {
+
+    },[searchTerm])
+
+
+    /**
+     * update the state with the value from the search input.
+     */
+    function handleChange() { 
+        setSearchTerm(inputField.current.value) 
+    }
+
     return (
         <div id="search-bar">
-            <label for="search-input">
+            <label htmlFor="search-input">
                 <div id="image-container">
                     <img src="https://img.icons8.com/ios-filled/50/000000/search--v1.png" />
 
                 </div>
+
                 <div ref={leftLine} id="left-line"></div>
-                <input ref={inputField} onFocus={changeState} onBlur={changeState} type="text" id="search-input" placeholder='Skill, Technology, Project...' autoComplete='off' />
+                <input onChange={handleChange} ref={inputField} onFocus={changeState} onBlur={changeState} type="text" id="search-input" placeholder='Skill, Technology, Project...' autoComplete='off' />
                 <div ref={rightLine} id="right-line"></div>
             </label>
         </div>)
