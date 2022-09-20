870a96f081c6fbe0d7b5ae9e761876147968c950
Karan Pal Singh <kdsgyt@gmail.com>
2022-09-20 13:02:39 -0400

    Contact us data is now dynamic

--- src/components/ContactUS/ContactUs.tsx
+++ src/components/ContactUS/ContactUs.tsx
@@ -1,13 +1,28 @@
+import firebase from 'firebase';
 import * as React from 'react';
 import './ContactUs.scss'
 import SocialMediaLinks from './SocialMediaLinks/SocialMediaLinks';
 
 export default function ContactUs() {
+    const [data, setData] = React.useState<any>({})
+    React.useEffect(() => {
+        firebase
+            .firestore()
+            .collection("/contact-us")
+            .get()
+            .then(res => res.forEach(item => {
+                console.log(item.data());
+                setData(item.data());
+            }))
+        return () => {
+        }
+    }, [])
+
     return (
         <section id="contact-us" className="display-flex">
             <h2>Contact Me</h2>
-            <p>If you’re hiring for a Frontend role, or have any questions,  please don’t hesitate to reach out!</p>
-            <h3><a href="mailto:karan@kdsg.media">karan@kdsg.media</a></h3>
+            <p>{data.contactUsText}</p>
+            <h3><a href={`mailto: ${data.email}`}>{data.email}</a></h3>
             <SocialMediaLinks />
         </section>
     )
